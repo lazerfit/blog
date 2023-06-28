4c73542d5dd4aa485cd59e213aec538efb6c907a
kim <redkafe98@gmail.com>
2023-06-28 14:07:34 +0900

    :sparkles: add searchPosts Test

---
+++ src/test/java/com/blog/domain/posts/PostsRepositoryImplTest.java
@@ -0,0 +1,56 @@
+package com.blog.domain.posts;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.blog.config.QuerydslConfig;
+import com.blog.web.dto.PostsResponseDto;
+import jakarta.persistence.EntityManager;
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+@DataJpaTest
+@Import(QuerydslConfig.class)
+class PostsRepositoryImplTest {
+
+    @Autowired
+    private EntityManager em;
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @BeforeEach
+    void insertMockData() {
+        IntStream.range(1,20).forEach(i ->
+            postsRepository.save(Posts.builder()
+                .title("제목"+i)
+                .content("내용"+i)
+                .build()));
+    }
+
+    @AfterEach
+    void teardown() {
+        postsRepository.deleteAll();
+        em.clear();
+    }
+
+    @Test
+    void test1() {
+        postsRepository.save(Posts.builder()
+            .title("테스트용 제목입니다.")
+            .content("내용입니다.")
+            .build());
+        PageRequest pageRequest = PageRequest.of(0, 6);
+        Page<PostsResponseDto> searchedPosts = postsRepository.getPostsByKeywordList(pageRequest, "테스트");
+
+        assertThat(searchedPosts.stream().toList()).hasSize(1);
+        assertThat(searchedPosts.stream().toList().get(0).getTitle()).isEqualTo("테스트용 제목입니다.");
+    }
+
+}
