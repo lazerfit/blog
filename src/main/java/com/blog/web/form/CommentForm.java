79910e97d687b4881bdd68c0b09b3205cc7c9ce3
sg kim <redkafe98@gmail.com>
2023-11-22 02:59:14 +0900

    update: update commentForm

--- src/main/java/com/blog/web/form/CommentForm.java
+++ src/main/java/com/blog/web/form/CommentForm.java
@@ -5,16 +5,22 @@ import jakarta.validation.constraints.NotNull;
 import java.util.Objects;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
 public class CommentForm {
 
-    @NotBlank(message = "이름은 필수입니다.") String username;
+    @Length(min = 1, max = 10, message = "작성자는 1~10글자로 입력해주세요.")
+    @NotBlank(message = "이름은 필수입니다.")
+    String username;
+
     @NotBlank(message = "내용은 필수입니다.") String content;
     Long parentId;
     @NotNull(message = "게시글 ID 는 필수입니다.") Long postId;
-    @NotBlank(message = "비밀번호는 필수입니다.") String password;
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    @Length(min = 4, max = 14, message = "비밀번호는 4~14글자로 입력해주세요.")
+    String password;
 
     public void setParentId(Long parentId) {
         this.parentId = Objects.requireNonNullElse(parentId, 0L);
