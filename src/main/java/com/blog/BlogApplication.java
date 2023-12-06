e2dd176c550919923625d716d8ff2cd7a9289165
sg kim <redkafe98@gmail.com>
2023-12-07 03:45:53 +0900

    Moved @EnableCaching to CacheConfig from BlogApplication
    The @EnableCaching annotation was moved from the `BlogApplication.java` class to the `CacheConfig.java` class. This is to further segregate the configuration related settings to their own specific config files, improving readability and maintenance of the code.

--- src/main/java/com/blog/BlogApplication.java
+++ src/main/java/com/blog/BlogApplication.java
@@ -2,10 +2,9 @@ package com.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableCaching
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class BlogApplication {
