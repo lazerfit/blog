6e86336f833248a193bd5ed99414e9ab1d55d0a3
sg kim <redkafe98@gmail.com>
2023-12-07 03:05:53 +0900

    Add Configuration annotation to CacheConfig
    The @Configuration annotation is added to the CacheConfig class. This change allows Spring to detect it automatically as a source of bean definitions, hence the necessity of this annotation. This improves the configuration management of the caching system in the application.

--- src/main/java/com/blog/config/CacheConfig.java
+++ src/main/java/com/blog/config/CacheConfig.java
@@ -5,7 +5,9 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class CacheConfig {
 
     @Bean
