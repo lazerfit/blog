b1dd2258fe22cc53162d04236a45fbb1d8f62650
sg kim <redkafe98@gmail.com>
2023-12-07 23:18:28 +0900

    delete comment

--- src/main/java/com/blog/domain/comments/CommentsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.blog.domain.comments;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentsRepository extends JpaRepository<Comment,Long> {
-    List<Comment> findByPostId(Long postId);
-}
--- src/main/java/com/blog/web/dto/comments/CommentPassword.java
+++
@@ -1,5 +0,0 @@
-package com.blog.web.dto.comments;
-
-public record CommentPassword(String password) {
-
-}
