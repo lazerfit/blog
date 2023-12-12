522819d5e3388490af254266132f9840007ecfb6
sg kim <redkafe98@gmail.com>
2023-12-12 18:20:49 +0900

    Remove userid from SiteUser classes
    The `userid` field has been removed from both `SiteUserPrincipal` and `SiteUserSecondary` classes as it was unnecessary. This change helps to streamline the code and improve maintainability.

--- src/main/java/com/blog/config/user/SiteUserPrincipal.java
+++ src/main/java/com/blog/config/user/SiteUserPrincipal.java
@@ -9,11 +9,9 @@ import org.springframework.security.core.userdetails.User;
 @Getter
 public class SiteUserPrincipal extends User {
 
-    private final Long userid;
 
     public SiteUserPrincipal(SiteUser user) {
         super(user.getEmail(), user.getPassword(), List.of(new SimpleGrantedAuthority("ROLE_ADMIN")));
-        this.userid= user.getId();
     }
 
 }
--- src/main/java/com/blog/config/user/SiteUserSecondary.java
+++ src/main/java/com/blog/config/user/SiteUserSecondary.java
@@ -7,10 +7,7 @@ import org.springframework.security.core.userdetails.User;
 
 public class SiteUserSecondary extends User {
 
-    private final Long userid;
-
     public SiteUserSecondary(SiteUser user) {
         super(user.getEmail(), user.getPassword(), List.of(new SimpleGrantedAuthority("ROLE_USER")));
-        this.userid= user.getId();
     }
 }
