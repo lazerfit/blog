2c9752972eb6a3276311303cc29c196c890cce96
sg kim <redkafe98@gmail.com>
2023-12-19 07:49:51 +0900

    BaseTimeEntity abstract class로 변경

--- src/main/java/com/blog/domain/posts/BaseTimeEntity.java
+++ src/main/java/com/blog/domain/posts/BaseTimeEntity.java
@@ -11,7 +11,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Getter
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
-public class BaseTimeEntity {
+public abstract class BaseTimeEntity {
 
     @CreatedDate
     private LocalDateTime generationTimeStamp;
